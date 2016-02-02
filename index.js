33e4553f8e99fe1c9b2ccd383802b6c05de2e0e9
Serviox19 <servio19@gmail.com>
2016-02-01 21:38:15 -0500

    started zoo object, but still need to import sql file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var prompt = require('prompt');
+
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -6,4 +8,20 @@ var connection = mysql.createConnection({
   database: 'zoo_db'
 });
 
-connection.connect();
+prompt.start();
+
+var zoo = {
+  welcome: function(){
+    console.log("Welcome to the Zoo And Friends App~!");
+  },
+  menu: function(){
+    console.log('Enter (A): -----> to Add a new animal to the Zoo!');
+    console.log('Enter (U): -----> to Update info on an animal in the Zoo!');
+    console.log('Enter (V): -----> to Visit the animals in the Zoo!');
+    console.log('Enter (D): -----> to Adopt an animal from the Zoo!');
+    console.log('Enter (Q): -----> to Quit and exit the Zoo!');
+  },
+  add: function(input_scope){
+    
+  }
+}
